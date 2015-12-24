42012cad6ea7c0273a10eb49f2ddeca6bc59e648
kaona <alain.armand@gmail.com>
2015-12-24 15:11:20 -0500

    Firebase/Knockout Binding/Search working
    Added Marker(place) to Place (function). Added back self.placeArray to
    house api retrieved data and push to self.placeList array

--- js/app.js
+++ js/app.js
@@ -1,51 +1,20 @@
 var vm;
 var map = document.getElementById ('map');
 
-var localPlaces = [
-  {
-    name: 'MATELEC S.A Electromenager',
-    latLng: {lat: 18.515732, lng: -72.293135},
-  },
-  
-  {
-    name: 'Thompson Electronics S.A.',
-    latLng: {lat: 18.5140218, lng: -72.2919172}
-  },
-  
-  {
-    name: 'Pages Jaunes Haiti',
-    latLng: {lat: 18.511007, lng: -72.2911917}
-  },
-
-  {
-    name: 'Marie de Petion-Ville',
-    latLng: {lat: 18.5098571, lng: -72.2882233},
-  },
-  
-  {
-    name: 'La Lorraine Boutique Hotel',
-    latLng: {lat: 18.5123482, lng: -72.2918561}
-  },
-  
-  {
-    name: 'Maison Acra - Petion-Ville',
-    latLng: {lat: 18.5127402, lng: -72.2886953},
-  }
-];
 
 
 var viewModel = function () {
 
       
   var self = this;
-  
-  // custom data object to make objects out of data in localPlaces and add data to it.
+
   self.placeArray = [];
-  self.markersArray =[];
+  
+  // observable data object to make objects show object coming from Firebase.
   
   self.placeList = ko.observableArray();
-  // build google map object, store in reference var
-  // var pv = {lat: 18.5128958, lng: -72.2939841};
+
+  // build google map object
 
   self.googleMap = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 18.5128958, lng: -72.2939841},
@@ -56,28 +25,26 @@ var viewModel = function () {
 
   // get data from Pages Jaunes Haiti database, push to localPlaces []
   var ref = new Firebase("https://crackling-fire-1105.firebaseio.com/business");
+
+  // get data from firebase.
+  function searchbiz() {
   ref.orderByChild("city").equalTo("Pétion-Ville").on("child_added", function(snapshot) {
       if (snapshot.val().hasOwnProperty('name') && snapshot.val().hasOwnProperty('latitude')) {
            var place = { accountid: snapshot.val().accountid, name: snapshot.val().name,
                    latLng: {lat: snapshot.val().latitude, lng: snapshot.val().longitude},
                  };
-            
-           self.placeList.push(new Place(place));
-           Marker(place);
-           self.placeList.push
-           // console.log(place.marker);
-           self.markersArray.push(place.marker)
-   }
+          //push data to array placeArray
+          self.placeArray.push(new Place(place));       
+          //push data to observable array placeList
+          self.placeList.push(place);
+      }
   // Attach an asynchronous callback to read the data at our posts reference
-
     }, function (errorObject) {
     console.log("The read failed: " + errorObject.code);
-  });
-
-  // localPlaces.forEach(function(place) {
-  //   self.placeArray.push(new Place(place));
-  // });
-
+    });
+  }
+  searchbiz();
+  
   // Previously had this place.marker code set as a createMarker function outside of this function and tried to 
   // call it inside of self.placeArray. function. It did not work because of
   // the map it was referring to was not defined inside the vm(i think). I put it
@@ -110,15 +77,14 @@ var viewModel = function () {
       setTimeout(function(){place.marker.setAnimation(null);}, 1450);
     });
     return place.marker;
-    markersArray.push(place.marker);
   };
 
   // self.placeList = ko.observableArray();
 
-  self.placeArray.forEach(function(place)  {
-    self.placeList.push(place);
-    console.log(self.placeList);
-  });
+  // self.placeArray.forEach(function(place)  {
+  //   self.placeList.push(place);
+  //   console.log(self.placeList);
+  // });
 
   // // Create a marker for each Place via the google maps api. The call takes a latlng and map id property at least.
   // // https://developers.google.com/maps/documentation/javascript/markers
@@ -152,21 +118,26 @@ var viewModel = function () {
     this.accountid = data.accountid;
     this.name = data.name;
     this.latLng = data.latLng;
+    this.marker = Marker(data);
     this.contentString = '<div><strong>' + this.name + '</strong></div>';
   }
 
   bounceUp = function(place) {
-    var i;
-    for (i = 0; i < self.markersArray.length; i++) {
-    var marker = self.markersArray[i];
-    if (place.accountid === marker.accountid) {break;} {
-      google.maps.event.trigger(marker, 'click');
-      }
-      console.log(marker);
-      console.log(place);
+      google.maps.event.trigger(place.marker, 'click');
+      console.log(place.marker);
+   }
+  // bounceUp = function(place) {
+  //   var i;
+  //   for (i = 0; i < self.markersArray.length; i++) {
+  //   var marker = self.markersArray[i];
+  //   if (place.accountid === marker.accountid) {
+  //     google.maps.event.trigger(marker, 'click');
+  //     }
+  //     console.log(marker);
+  //     console.log(place);
 
-    }
-  }
+  //   }
+  // }
 
 };
 
